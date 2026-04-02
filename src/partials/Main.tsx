461d5c5f5e3daa10e536ab1b93c67d87ff2a16aa
stuNero <max.vemic@student.nbi-handelsakademin.se>
2026-04-02 10:08:01 +0200

    add styling and routing

--- src/partials/Main.tsx
+++ src/partials/Main.tsx
@@ -1,9 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import GamePage from "../pages/GamePage";
+import LobbyPage from "../pages/LobbyPage";
 export default function Main() {
-  return <main>
+  return <main
+    className="
+      bg-amber-500/30
+      bg-linear-to-t
+      max-w-7xl
+      min-h-screen
+      pt-40
+      items-center
+      justify-center
+      align-center
+    ">
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/lobby" element={<LobbyPage />} />
+      <Route path="/game" element={<GamePage />} />
     </Routes>
   </main>;
 };
