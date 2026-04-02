c93447f252005018053ca3a695436289dfe26c48
stuNero <max.vemic@student.nbi-handelsakademin.se>
2026-04-02 10:44:16 +0200

    fix header styling

--- src/partials/Header.tsx
+++ src/partials/Header.tsx
@@ -3,16 +3,16 @@ export default function Header() {
   return <header
     className="
     w-full
-    h-35
+    h-30
     bg-stone-400
     backdrop-blur-lg
     fixed z-500
     flex flex-col
-    items-center
+    justify-around
     top-0
     ">
     <Link to="/">
-      <img src="lexiconquest-logo.png" className="px-5 pb-5 py-2 h-[75%] w-auto"></img>
+      <img src="lexiconquest-logo.png" className="px-5 pb-5 py-2 h-30 w-auto"></img>
     </Link>
   </header>;
 };
