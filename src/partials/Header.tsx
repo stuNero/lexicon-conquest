f4208d8340e1df7dec065211da6ef96294e3570c
stuNero <max.vemic@student.nbi-handelsakademin.se>
2026-04-02 10:07:32 +0200

    add header styling

--- src/partials/Header.tsx
+++ src/partials/Header.tsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
 export default function Header() {
-  return <>
-    <h1>header</h1>
-  </>;
+  return <header
+    className="
+    w-full
+    h-35
+    bg-stone-400
+    backdrop-blur-lg
+    fixed z-500
+    flex flex-col
+    items-center
+    top-0
+    ">
+    <Link to="/">
+      <img src="lexiconquest-logo.png" className="px-5 pb-5 py-2 h-[75%] w-auto"></img>
+    </Link>
+  </header>;
 };
