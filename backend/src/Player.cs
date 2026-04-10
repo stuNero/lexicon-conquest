492c9fb35b64938c40651e3314f4326c79e03f32
stuNero <max.vemic@student.nbi-handelsakademin.se>
2026-04-10 19:20:50 +0200

    refactored guid

--- backend/src/Player.cs
+++ backend/src/Player.cs
@@ -4,12 +4,11 @@ namespace backend;
 
 public class Player
 {
-  public Guid id { get; set; }
+  public Guid id { get; set; } = Guid.NewGuid();
   public string UserName { get; set; }
   public bool Ready { get; set; }
   public Player(string userName, bool ready)
   {
-    id = Guid.NewGuid();
     UserName = userName;
     Ready = ready;
   }
