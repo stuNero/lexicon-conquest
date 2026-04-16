387f4310665b23903efbf1e35d657d47ed4c6fdd
stuNero <max.vemic@student.nbi-handelsakademin.se>
2026-04-16 14:06:59 +0200

    commented out header import to ignore warning about header being removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import Header from "./partials/Header";
+// import Header from "./partials/Header";
 import Main from "./partials/Main";
 function App() {
   return (
