18c5ecee1f1ded42e302b7e153e01c0af5a7e161
amir247 <amir_hamza@outlook.com>
2026-04-21 14:30:10 +0200

    Fix casing conflict for fetchJson

--- src/utils/fetchjson.tsx
+++
@@ -1,7 +0,0 @@
-export default async function fetchJson<T>(url: string, options = {}): Promise<T> {
-  let response = await fetch(url, options);
-  if (!response.ok)
-    throw new Error(await response.text());
-  let data = await response.json();
-  return data;
-}
