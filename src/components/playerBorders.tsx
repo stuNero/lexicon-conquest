2ec10a9fe9bb13789af57a67ea850c332a85ec26
stuNero <max.vemic@student.nbi-handelsakademin.se>
2026-04-23 17:20:45 +0200

    Broke playercard into components, add tailwindstyle file
    Co-authored-by: OliverApel96 <oliver.apelqvist@hotmail.se>
    Co-authored-by: Haviet Kok <havietkok-sys@users.noreply.github.com>
    Co-authored-by: amirhamza247 <amirhamza247@users.noreply.github.com>

--- src/components/playerBorders.tsx
+++
@@ -1,6 +0,0 @@
-export const playerBorders = [
-  "border-red-500/50",
-  "border-blue-500/50",
-  "border-emerald-500/50",
-  "border-amber-500/50",
-];
--- src/components/playerColors.tsx
+++
@@ -1,6 +0,0 @@
-export const playerColors = [
-  "bg-red-700",
-  "bg-blue-700",
-  "bg-emerald-700",
-  "bg-amber-700",
-];
